b88494f73231a4976532a7ab7d82056a95841521
TrawlWizrt <kubis.padangpasir@gmail.com>
2019-04-22 16:25:10 +0700

    Manage plugin dependencies.

--- Source/AJClipperPlugin/Private/AJCUtilityLibrary.cpp
+++ Source/AJClipperPlugin/Private/AJCUtilityLibrary.cpp
@@ -26,6 +26,7 @@
 //
 
 #include "AJCUtilityLibrary.h"
+#include "Poly/GULPolyUtilityLibrary.h"
 
 ClipperLib::ClipType FAJCClipperConfig::GetClipType() const
 {
@@ -149,21 +150,5 @@ int32 FAJCUtilityLibrary::IsPointOnPolygons(const FVector2D& Point, const FAJCPo
 
 float FAJCUtilityLibrary::GetVectorPathArea(const FAJCVectorPathRef& VectorPath)
 {
-    const TArray<FVector2D>& Points(VectorPath.Data);
-    int32 PointCount = Points.Num();
-    
-    if (PointCount < 3)
-    {
-        return 0.f;
-    }
-    
-    float a = 0;
-    
-    for (int32 i=0, j=PointCount-1; i<PointCount; ++i)
-    {
-      a += (Points[j].X + Points[i].X) * (Points[j].Y - Points[i].Y);
-      j = i;
-    }
-    
-    return -a * 0.5;
+    return UGULPolyUtilityLibrary::GetArea(VectorPath.Data);
 }
